6a85d4cecd625e7e2fda7a85bd9fbea1b027d67c
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-09 23:20:21 +0300

    material ui installation

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SideBarData } from './SideBarData'
 
 function SideBar() {
     return (
         <div className="side-bar">
-            
+           <ul>
+           {SideBarData.map((value, key) =>{
+               return 
+               <li key={key}>{""}
+               <div>{value.icon}</div>
+               <li>{value.link}</li>
+               </li>
+           })}
+           </ul>
         </div>
     )
 }
