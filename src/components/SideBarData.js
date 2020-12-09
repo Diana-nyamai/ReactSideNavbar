917020624f1c5ad767685682d4710da6f960fa5a
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-09 23:06:41 +0300

    sidebardata update

---
+++ src/components/SideBarData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const SideBarData =[
+    {
+        name:'Home',
+        icon:'',
+        link:''
+    },
+    {
+        name:'Home',
+        icon:'',
+        link:''
+    },
+] 
+  
+
