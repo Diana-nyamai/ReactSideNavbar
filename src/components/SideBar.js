61322e007687158dcc91c208b72a2b6515fac992
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-11 12:47:37 +0300

    update

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,7 +7,10 @@ function SideBar() {
            <ul className="sidebarlist">
            {SideBarData.map((value, key) =>{
                return(
-               <li key={key} onClick={()=>{window.location.pathname = value.linkc}} className="row">
+               <li key={key}
+                id= {window.location.pathname == value.link? "active": ""}
+                onClick={()=>{window.location.pathname = value.linkc}}
+                 className="row">
                <div id="icon">{value.icon}</div>
                <div id="name">{value.name}</div>
                </li>
