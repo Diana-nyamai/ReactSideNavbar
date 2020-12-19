00809c95b6c266fce928daa6082158cad94e19b0
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-19 11:10:31 +0300

    sidebar changes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,9 +7,11 @@ function SideBar() {
            <ul className="sidebarlist">
            {SideBarData.map((value, key) =>{
                return(
+                
                <li key={key}
                 id= {window.location.pathname == value.link? "active": ""}
                 onClick={()=>{window.location.pathname = value.link}}
+
                  className="row">
                <div id="icon">{value.icon}</div>
                <div id="name">{value.name}</div>
